956005994d6910f2063e13094afa2a3ae344d5f6
Super-D-prog <delarosadarihan04@gmail.com>
2026-09-17 22:12:15 -0400

    Set Woodbridge NJ as default weather location

--- modules/config.py
+++ modules/config.py
@@ -10,3 +10,4 @@ class Settings:
     history_limit: int = int(os.getenv("OMEGA_HISTORY_LIMIT", "20"))
     request_timeout: int = int(os.getenv("OMEGA_REQUEST_TIMEOUT", "120"))
     data_dir: str = os.getenv("OMEGA_DATA_DIR", "data")
+    home_location: str = os.getenv("OMEGA_HOME_LOCATION", "Woodbridge, New Jersey")
